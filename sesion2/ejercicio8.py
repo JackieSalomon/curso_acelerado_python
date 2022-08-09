37e15c3a40a66619c0f34fa1666df9a9226b2180
Replit user <>
2022-08-09 16:34:52 +0000

    Ejercicio de la segunda sesion

---
+++ sesion2/ejercicio8.py
@@ -0,0 +1,16 @@
+'''
+*********** Curso de programación acelerada en Python ************
+Date 04-08-2022
+File: sesion2/ejercicio8.py
+Autor: jackie
+Action: detecta numero negativos
+'''
+numero = int(input("Escriba un número: "))
+if numero < 0:
+    print("¡El número es negativo!")
+   
+else:
+    if numero > 0:
+       print("el número es positivo")
+    else:
+      print("el número es cero")
---
+++ sesion2/ejercicio9.py
@@ -0,0 +1,14 @@
+'''
+*********** Curso de programación acelerada en Python ************
+Date 09-08-2022
+File: sesion2/ejercicio9.py
+Autor: jackie
+Action: contraseña
+'''
+PSW = "IN"
+cts = input("Escriba su contraseña: ")
+if cts == PSW:
+    print("¡bienvenido al sistema!")
+   
+else:
+     print("¡revise su contraseña!")
